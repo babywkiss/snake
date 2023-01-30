94fb6ccfbaa4989b7e5d1a8fe9bd4750c49156a3
Maxim Kozhukh <wkiskas@Maxims-MacBook-Air.local>
2023-01-30 14:09:36 +0300

    not working but good

--- index.js
+++ index.js
@@ -3,39 +3,35 @@ const COLORS = {
   GREEN: '\x1b[42m',
   RED: '\x1b[41m',
   YELLOW: '\x1b[43m',
+  WHITE: '\x1b[47m',
 }
-const BLOCK = COLORS.YELLOW + ' ' + COLORS.RESET 
+const WHITE_BLOCK = COLORS.WHITE + '  ' + COLORS.RESET;
+const YELLOW_BLOCK = COLORS.YELLOW + '  ' + COLORS.RESET;
+const GREEN_BLOCK = COLORS.GREEN + '  ' + COLORS.RESET;
 
-const padCenter = (str, width) => str.padStart((str.length + width) / 2).padEnd(width);
+const padCenter = (str, width, space) => str.padStart((str.length + width) / 2, space).padEnd(width, space);
+
 const getField = (xBound, yBound) => Array.from({length: yBound}, _ => 
-  Array.from({length: xBound}, _ => ' '));
+  Array.from({length: xBound}, _ => '  '));
 
 const formatField = (field) => {
-  const xLine = BLOCK.repeat(field[0].length * 2 + 1) + '\n';
-  str = xLine;
-  str += field.map(line => BLOCK + line.join(" ") + BLOCK).join("\n");
-  str += '\n' + xLine;
-  return str;
+  const xLine = WHITE_BLOCK.repeat(field[0].length + 2) + '\n';
+  const body = field.map(line => WHITE_BLOCK + line.join("") + WHITE_BLOCK).join("\n") + '\n';
+  return xLine + body + xLine;
 };
 
-const fillSnake = (field, snake) => snake.forEach(([x, y]) => 
-  field[y][x] = COLORS.GREEN + " " + COLORS.RESET);
+const fillSnake = (field, snake) => snake.forEach(([x, y], i) => 
+  field[y][x] = i === snake.length - 1 ? YELLOW_BLOCK : GREEN_BLOCK);
+
+const fillApples = (field, apples) => apples.forEach(([x, y]) => 
+  field[y][x] = COLORS.RED + '  ' + COLORS.RESET);
 
 const moveSnake = (snake, xDir, yDir) => {
   snake.push(snake.slice(-1).map(([x, y]) => [x + xDir, y + yDir])[0]); 
 };
 
-const spawnApple = (apples, xBound, yBound) => {
-  apples.push([Math.floor(Math.random() * xBound), Math.floor(Math.random() * yBound)]);
-};
-
-const fillApples = (field, apples) => apples.forEach(([x, y]) => 
-  field[y][x] = COLORS.RED + ' ' + COLORS.RESET);
-
-const collectApple = (apples, hx, hy) => {
-  const filtered = apples.filter(([x, y]) => !(x === hx && y === hy));
-  const collected = filtered.length !== apples.length
-  return [filtered, collected];
+const getBoundedBlock = (xBound, yBound) => {
+  return [Math.floor(Math.random() * xBound), Math.floor(Math.random() * yBound)];
 };
 
 const checkSnake = (snake, hx, hy, xBound, yBound) => {
@@ -47,10 +43,11 @@ const checkSnake = (snake, hx, hy, xBound, yBound) => {
 };
 
 
-const formatScore = (fps, prob, score, xBound, yBound, seconds) => {
-  const xLine = "-".repeat(40) + '\n';
+const formatScore = (tickrate, prob, score, xBound, yBound, seconds) => {
+  const xLine = WHITE_BLOCK.repeat(40) + '\n';
+  const formatLine = s => WHITE_BLOCK + padCenter(s, 38, WHITE_BLOCK) + WHITE_BLOCK + '\n';
   let str = xLine + 
-  "|" + padCenter(`FPS: ${fps}`, 38) + "|\n" +
+  "|" + padCenter(`FPS: ${tickrate}`, 38) + "|\n" +
   "|" + padCenter(`Probability: ${prob}`, 38) + "|\n" +
   "|" + padCenter(`Score: ${score}`, 38) + "|\n" + 
   "|" + padCenter(`Field: ${xBound}x${yBound}`, 38) + "|\n" + 
@@ -59,33 +56,36 @@ const formatScore = (fps, prob, score, xBound, yBound, seconds) => {
   return str;
 }
 
-const game = (fps, prob, xBound, yBound) => {
-  const start = Date.now();
-  const speed = 1000 / fps;
+const game = (tickrate, xBound, yBound) => {
+  const time_start = Date.now();
+  const speed = 1000 / tickrate;
   let prevKey = '';
   const snake = [[1, 1]];
   let [xDir, yDir] = [0, 1];
-  let apples = [];
+  let apple = getBoundedBlock(xBound, yBound);
   let grow = false;
 
   setInterval(() => {
     console.clear();
     moveSnake(snake, xDir, yDir);
     const [hx, hy] = snake.slice(-1)[0];
-    if (!checkSnake(snake, hx, hy, xBound, yBound)) {
-      console.log(formatScore(fps, prob, snake.length, xBound, yBound, (Date.now() - start) / 1000));
+    if (!checkSnake(snake, hx, hy, xBound, yBound, stones)) {
+      console.log(formatScore(tickrate, prob, snake.length, xBound, yBound, (Date.now() - time_start) / 1000));
       process.exit();
     }
     [apples, grow] = collectApple(apples, hx, hy);
     if (!grow) snake.shift();
     grow = false;
-    if (Math.floor(Math.random() * 100) < prob) spawnApple(apples, xBound, yBound);
+    spawnApple(apples, xBound, yBound);
     const field = getField(xBound, yBound);
+    // experiment
+    stones.forEach(([x, y]) => field[y][x] = COLORS.WHITE + '  ' + COLORS.RESET);
+    // experiment
     fillApples(field, apples);
     fillSnake(field, snake);
     console.log(
       padCenter(`Score: ${'\x1b[32m'}${snake.length} Apples${COLORS.RESET}`, xBound * 2 + 1) +
-      `\nPassed: ${(Date.now() - start) / 1000} seconds.`
+      `\nPassed: ${(Date.now() - time_start) / 1000} seconds.`
     );
     console.log(formatField(field));
   }, speed)
@@ -98,10 +98,10 @@ const game = (fps, prob, xBound, yBound) => {
       '\033[B': 'd',
       '\033[D': 'l',
       '\033[C': 'r',
-      'j': 'd',
-      'k': 'u',
-      'h': 'l',
-      'l': 'r',
+      's': 'd',
+      'w': 'u',
+      'a': 'l',
+      'd': 'r',
     }
     const key = keys[data.toString()];
     if (!key || key === prevKey) return;
@@ -113,8 +113,8 @@ const game = (fps, prob, xBound, yBound) => {
   })
 };
 
-const FPS = 7;
-const PROB = 27;
-const XBOUND = 27;
-const YBOUND = 27;
+const FPS = 10;
+const PROB = 5;
+const XBOUND = 21;
+const YBOUND = 21;
 game(FPS, PROB, XBOUND, YBOUND);
